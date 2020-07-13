303064c0dab9ab513d3f0486537d65c90767d691
leonid.tankilevich <leonid.tankilevich@shtormtech.ru>
2020-07-14 01:34:10 +0300

    yo

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'production',
+	entry: './src/index.ts',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'index.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'React App with TypeScript',
+			template: './src/assets/index.html',
+			minify: false
+		}),
+	],
+	externals: {
+		"react": "React",
+		"react-dom": "ReactDOM"
+	}
+}
